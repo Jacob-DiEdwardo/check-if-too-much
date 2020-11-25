e060f95c3a0a668b2d65949a9c2bacb5033261fc
JacobDiEdwardo <jacob.diedwardo@gmail.com>
2020-11-24 23:42:39 -0500

    Assignment 1 Solution

--- module-1-solution/js/app.js
+++ module-1-solution/js/app.js
@@ -2,36 +2,29 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+.controller('LunchCheckController', ['$scope', LunchCheckController]);
 
-LunchCheckController.$inject = ['$scope']
 function LunchCheckController($scope) {
-    $scope.placeholder = "list comma separated dishes you usually have for lunch";
-    $scope.lunch = "";
+  $scope.placeholder = 'list comma-separated dishes you usually have for lunch';
+  $scope.lunch = '';
+  $scope.showMessage = false;
+  $scope.message = '';
+  $scope.success = false;
+  $scope.error = false;
 
-
-    $scope.lunchChecker = function(takes in the entry) {
-
-    };
+  $scope.checkLunch = function() {
+    var lunchArray = $scope.lunch.split(',').filter(function(item) {return item.match(/\w+/g)});
+    if (lunchArray.length === 0) {
+      $scope.message = 'Please enter data first.';
+      $scope.error = true;
+      $scope.success = false;
+    } else {
+      $scope.message = lunchArray.length <= 3 ? 'Enjoy!' : 'Too much.';
+      $scope.success = true;
+      $scope.error = false;
+    }
+    $scope.showMessage = true;
+  };
 };
 
 })();
-
-
-$scope.lunchConverter = function(takes in the entry) {
-store entry in a variable
-convert that variable into an Array
-return the array 
-};
-
-$scope.lunchChecker = function(takes in the array) {
-loop over each item in the array and count them
-check if the number of items is less than or equal to 3
-if so, return "Enjoy"
-if not, return "too much"
-};
-
-$scope.lunchConverter = function(lunch) {
-    let lunchArray = lunch.split();
-    return lunchArray;
-};
